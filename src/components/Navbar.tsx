f3a881cc23683143b787b1d1058fe3910d245498
Josh <jvader.megamind@gmail.com>
2025-08-07 00:17:17 -0500

    Relative paths

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ import NavButton from './NavButton';
 function Navbar() {
   return (
     <div className="flex">
-      <NavButton url='/' search="?currentTab=home" label="Home" />
-      <NavButton url='/' search="?currentTab=lsb" label="LSB" />
-      <NavButton url='/' search="?currentTab=about" label="About" />
+      <NavButton url='./' search="?currentTab=home" label="Home" />
+      <NavButton url='./' search="?currentTab=lsb" label="LSB" />
+      <NavButton url='./' search="?currentTab=about" label="About" />
     </div>
   );
 }
