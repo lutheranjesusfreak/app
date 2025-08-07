7e44ad3a13e823b866f4bacda019e0c93ebf6958
Josh <jvader.megamind@gmail.com>
2025-08-07 01:01:31 -0500

    bcp test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ function Navbar() {
     <div className="flex">
       <NavButton url='app/' search="" label="Home" />
       <NavButton url='app/' search="lsb" label="LSB" />
+      <NavButton url='app/' search="bcp" label="BCP" />
       <NavButton url='app/' search="about" label="About" />
     </div>
   );
